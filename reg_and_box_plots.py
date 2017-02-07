b772f5f5e2365be6439f2a995d0bc06181018353
Jonas <lowrin@users.noreply.github.com>
2017-02-07 19:14:49 +0100

    Create reg_and_box_plots.py

---
+++ reg_and_box_plots.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid")
+
+
+from scipy import stats
+
+# input data
+r1 = np.array([4,3,2,5,5,3])
+r2 = np.array([1,2,3,0,0,2])
+
+# constant value in every row
+c1 = np.full(len(r1),"r1",dtype=object)
+c2 = np.full(len(r2),"r2",dtype=object)
+
+# create the dataframes
+df = pd.DataFrame({'Value': r1,"Type":c1})
+df2 = pd.DataFrame({'Value': r2,"Type":c2})
+
+# make one dateframe
+df = df.append(df2,ignore_index=True)
+
+print(df.head())
+print(df.info())
+
+fig, axs = plt.subplots(ncols=3)
+
+# create a boxplot
+sns.boxplot(data=df,x="Type", y="Value",ax=axs[2])
+sns.swarmplot(data=df,x="Type", y="Value",color=".25",ax=axs[2])
+
+
+
+
+sns.regplot(x=df[df['Type']=="r1"].index.values,y=df[df['Type']=="r1"].Value, data=df, ax=axs[0])
+sns.regplot(x=df[df['Type']=="r2"].index.values,y=df[df['Type']=="r2"].Value, data=df, ax=axs[1])
+ax=axs[1].set_title("Regplot R2")
+ax=axs[0].set_title("Regplot R1")
+ax=axs[2].set_title("Boxplot")
+sns.plt.show()
